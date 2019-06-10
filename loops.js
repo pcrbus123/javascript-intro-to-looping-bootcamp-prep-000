9047db7983f5556ea89ab7d2008230371eee081e
Pnina Breslav <pcrbus123@gmail.com>
2019-06-10 15:10:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -13,7 +13,10 @@ function whileLoop(n){
     console.log(--n)
   } return 'done'
 }
-
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 function doWhileLoop(num){
 do{
   console.log("I run once regardless.");

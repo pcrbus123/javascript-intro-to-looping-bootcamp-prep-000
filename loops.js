47b529eb6bd9e59ab448186dc3c7d34a5ea27a34
Pnina Breslav <pcrbus123@gmail.com>
2019-06-10 15:05:17 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,7 @@
+function forLoop(array){
+  for( let i=0 ;i<25; i++){
+    if (i == 1){
+      array += "I am 1 strange loop."
+    } else { array += "I am ${i} strange loop."
+  }
+}return forLoop()
